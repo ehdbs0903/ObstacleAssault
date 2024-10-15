6b1e15c0ffd7d5355c57c19af7e6aab3c63438f3
Doyun Kim <ehdbs8747@naver.com>
2024-10-15 18:28:39 +0900

    feat(AMovingPlatform): add RotationPlatform
    - Added functionality for RotationPlatform
    - Adjusted initial platform values for improved behavior

--- Source/ObstacleAssault/MovingPlatform.cpp
+++ Source/ObstacleAssault/MovingPlatform.cpp
@@ -25,6 +25,7 @@ void AMovingPlatform::Tick(float DeltaTime)
 	Super::Tick(DeltaTime);
 
 	MovePlatform(DeltaTime);
+	RotatePlatform(DeltaTime);
 }
 
 void AMovingPlatform::MovePlatform(float DeltaTime)
@@ -42,6 +43,11 @@ void AMovingPlatform::MovePlatform(float DeltaTime)
 		SetActorLocation(CurrentLocation);
 	
 	}
+}
+
+void AMovingPlatform::RotatePlatform(float DeltaTime)
+{
+	AddActorLocalRotation(RotationVelocity * DeltaTime);
 }
 
 bool AMovingPlatform::ShouldPlatformReturn() const
